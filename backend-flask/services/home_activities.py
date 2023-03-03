e2839598ea8bd73d07327d2ea1559089bbb2eb9f
Brian Gitau <miiri.brian@gmail.com>
2023-03-03 14:42:07 +0000

    update for week 1

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,10 +5,10 @@ class HomeActivities:
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
       'handle':  'Andrew Brown',
-      'message': 'Cloud is fun!',
+      'message': 'Kuna Nduru Gizani!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
-      'likes_count': 5,
+      'likes_count': 900,
       'replies_count': 1,
       'reposts_count': 0,
       'replies': [{
